cb5111f1b7c4e63a2c12f118b3a75d221a1dd08d
최병욱 <esc5221@gmail.com>
2024-02-17 03:20:08 +0900

    feat: add post like api

--- post/views.py
+++ post/views.py
@@ -4,6 +4,8 @@ from typing import List
 from ninja import File, Router, Schema
 from ninja.files import UploadedFile
 
+from django.db import transaction
+
 from post.models import Post
 from user.models import User
 
@@ -15,12 +17,20 @@ class PostCreateSchema(Schema):
 
 
 class PostSchema(Schema):
+    id: int
     text: str
     image: str
     author_id: int
     created_at: datetime.datetime
     updated_at: datetime.datetime
     like_count: int
+    is_liked: bool
+
+    @staticmethod
+    def resolve_is_liked(obj):
+        if not hasattr(obj, "_user_likes_post"):
+            return False
+        return bool(obj._user_likes_post)
 
 
 @router.post(
@@ -43,14 +53,45 @@ def post_create(request, params: PostCreateSchema, image: UploadedFile = File(..
     auth=None,
 )
 def post_get(request, post_id: int):
+    user = request.user
     post = Post.objects.get(id=post_id)
+    post._user_likes_post = bool(user.likes.filter(post=post).exists())
     return post
 
 
-@router.post(
+@router.get(
+    "/{int:post_id}/like",
+    response={200: PostSchema},
+    auth=None,
+)
+@transaction.atomic
+def post_like(request, post_id: int):
+    post = Post.objects.get(id=post_id)
+    user = request.user
+    if user.likes.filter(post=post).exists():
+        user.likes.filter(post=post).delete()
+        post.like_count -= 1
+        post._user_likes_post = False
+    else:
+        user.likes.create(post=post)
+        post.like_count += 1
+        post._user_likes_post = True
+    post.save()
+    return post
+
+
+@router.get(
     "/list",
     response={200: List[PostSchema]},
 )
 def post_list(request):
-    posts = Post.objects.all()
+    from django.db.models import Prefetch
+
+    posts = Post.objects.all().prefetch_related(
+        Prefetch(
+            "likes",
+            queryset=request.user.likes.all(),
+            to_attr="_user_likes_post",
+        )
+    )
     return posts
