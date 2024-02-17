12488426b79ca2e415e7bf295821f3fc892e60b8
최병욱 <esc5221@gmail.com>
2024-02-17 19:52:57 +0900

    fix: change user profile_image to image with ImageFieldC

--- user/models.py
+++ user/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from common.upload import convert_filename
+
+
+def upload_to_func(instance, filename):
+    prefix = "uploads/user_images/"
+    return prefix + convert_filename(filename)
+
 
 class User(AbstractUser):
     first_name = None
@@ -10,4 +17,4 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ["username"]
 
     email = models.EmailField(_("email address"), unique=True)
-    profile_image_url = models.URLField(_("profile image url"), null=True)
+    image = models.ImageField(upload_to=upload_to_func, blank=True, null=True)
